9a4e86e450f6e89d33619b9b0815278bf5aa9fa6
povidllo <kuzminovm78@gmail.com>
2024-09-09 20:59:10 +0700

    first

---
+++ Task_1_1_1/src/main/java/ru/nsu/kuzminov/Main.java
@@ -0,0 +1,11 @@
+package ru.nsu.kuzminov;
+
+public class Main {
+    public static int aga(int a)
+    {
+        return a;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Task_1_1_1/src/test/java/ru/nsu/kuzminov/MainTest.java
@@ -0,0 +1,13 @@
+package ru.nsu.kuzminov;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void test() {
+        assertEquals(4, Main.aga(4));
+//        Main.aga(3);
+    }
+}
