0bab1593edbcb11f8125d59eae0ca125266f3e78
povidllo <kuzminovm78@gmail.com>
2024-09-12 11:14:27 +0700

    execute.sh

--- Task_1_1_1/src/test/java/ru/nsu/kuzminov/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/kuzminov/MainTest.java
@@ -6,54 +6,81 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
-    void test() {
+    void test1() {
         int[] testArr = {5, 4, 3, 2, 1};
         int[] expArr = {1, 2, 3, 4, 5};
         Main.heapSort(testArr);
         assertArrayEquals(expArr, testArr);
+    }
 
-        testArr = new int[]{1, 2, 3, 4, 5};
-        expArr = new int[]{1, 2, 3, 4, 5};
+    @Test
+    void test2() {
+        int[] testArr = new int[]{1, 2, 3, 4, 5};
+        int[] expArr = new int[]{1, 2, 3, 4, 5};
         Main.heapSort(testArr);
         assertArrayEquals(expArr, testArr);
+    }
 
-        testArr = new int[]{};
-        expArr = new int[]{};
+    @Test
+    void test3() {
+        int[] testArr = new int[]{};
+        int[] expArr = new int[]{};
         Main.heapSort(testArr);
         assertArrayEquals(expArr, testArr);
+    }
 
-        testArr = new int[]{1};
-        expArr = new int[]{1};
+    @Test
+    void test4() {
+        int[] testArr = new int[]{1};
+        int[] expArr = new int[]{1};
         Main.heapSort(testArr);
         assertArrayEquals(expArr, testArr);
+    }
 
-        testArr = new int[]{1, 1, 1, 1, 1, 1, 1};
-        expArr = new int[]{1, 1, 1, 1, 1, 1, 1};
+    @Test
+    void test5() {
+        int[] testArr = new int[]{1, 1, 1, 1, 1, 1, 1};
+        int[] expArr = new int[]{1, 1, 1, 1, 1, 1, 1};
         Main.heapSort(testArr);
         assertArrayEquals(expArr, testArr);
+    }
 
-        testArr = new int[]{1, 1, 1, 1, 2, 2, 2, 2};
-        expArr = new int[]{1, 1, 1, 1, 2, 2, 2, 2};
+    @Test
+    void test6() {
+        int[] testArr = new int[]{1, 1, 1, 1, 2, 2, 2, 2};
+        int[] expArr = new int[]{1, 1, 1, 1, 2, 2, 2, 2};
         Main.heapSort(testArr);
         assertArrayEquals(expArr, testArr);
+    }
 
-        testArr = new int[]{2, 2, 2, 2, 1, 1, 1, 1};
-        expArr = new int[]{1, 1, 1, 1, 2, 2, 2, 2};
+    @Test
+    void test7() {
+        int[] testArr = new int[]{2, 2, 2, 2, 1, 1, 1, 1};
+        int[] expArr = new int[]{1, 1, 1, 1, 2, 2, 2, 2};
         Main.heapSort(testArr);
         assertArrayEquals(expArr, testArr);
+    }
 
-        testArr = new int[]{-1, -2, -3, -4, -5};
-        expArr = new int[]{-5, -4, -3, -2, -1};
+    @Test
+    void test8() {
+        int[] testArr = new int[]{-1, -2, -3, -4, -5};
+        int[] expArr = new int[]{-5, -4, -3, -2, -1};
         Main.heapSort(testArr);
         assertArrayEquals(expArr, testArr);
+    }
 
-        testArr = new int[]{-5, -4, -3, -2, -1};
-        expArr = new int[]{-5, -4, -3, -2, -1};
+    @Test
+    void test9() {
+        int[] testArr = new int[]{-5, -4, -3, -2, -1};
+        int[] expArr = new int[]{-5, -4, -3, -2, -1};
         Main.heapSort(testArr);
         assertArrayEquals(expArr, testArr);
+    }
 
-        testArr = new int[]{5, 7, 2, 1, 2, 4, 5};
-        expArr = new int[]{1, 2, 2, 4, 5, 5, 7};
+    @Test
+    void test10() {
+        int[] testArr = new int[]{5, 7, 2, 1, 2, 4, 5};
+        int[] expArr = new int[]{1, 2, 2, 4, 5, 5, 7};
         Main.heapSort(testArr);
         assertArrayEquals(expArr, testArr);
 
