237348e5a73fa0f95af0fc8ea13f283991e58900
povidllo <kuzminovm78@gmail.com>
2024-09-12 11:43:22 +0700

    a

--- Task_1_1_1/src/main/java/ru/nsu/kuzminov/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/kuzminov/Main.java
@@ -49,10 +49,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int[] testArr = {1, 2, 3, 4, 5, 6, 7};
-        Main.heapify(testArr, 2, testArr.length);
-        for (int i : testArr) {
-            System.out.println(i);
-        }
+//        int[] testArr = {1, 2, 3, 4, 5, 6, 7};
+//        Main.heapify(testArr, 2, testArr.length);
+//        for (int i : testArr) {
+//            System.out.println(i);
+//        }
     }
 }
