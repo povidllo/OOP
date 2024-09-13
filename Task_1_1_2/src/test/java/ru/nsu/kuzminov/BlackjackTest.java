b147a6380b9c4f93f6836df6516fa0ccae0de2ef
povidllo <kuzminovm78@gmail.com>
2024-09-13 14:03:08 +0700

    aga

--- Task_1_1_2/src/test/java/ru/nsu/kuzminov/BlackjackTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/kuzminov/BlackjackTest.java
@@ -2,12 +2,9 @@ package ru.nsu.kuzminov;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 class BlackjackTest {
-
     @Test
     void testConstructor() {
         Blackjack game = new Blackjack();
--- Task_1_1_2/src/test/java/ru/nsu/kuzminov/CardTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/kuzminov/CardTest.java
@@ -2,7 +2,7 @@ package ru.nsu.kuzminov;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CardTest {
     @Test
