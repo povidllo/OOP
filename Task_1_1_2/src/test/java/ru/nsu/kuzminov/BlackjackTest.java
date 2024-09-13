3ec575ed53220eaf63c7db594f4fdacc7737f692
povidllo <kuzminovm78@gmail.com>
2024-09-13 13:41:34 +0700

    test

--- Task_1_1_2/src/test/java/ru/nsu/kuzminov/BlackjackTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/kuzminov/BlackjackTest.java
@@ -1,8 +1,9 @@
 package ru.nsu.kuzminov;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class BlackjackTest {
 
