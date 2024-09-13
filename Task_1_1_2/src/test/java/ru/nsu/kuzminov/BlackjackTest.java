333695333b0ffca63626b40f07c7c30ef6208fda
povidllo <kuzminovm78@gmail.com>
2024-09-13 13:44:26 +0700

    static

--- Task_1_1_2/src/test/java/ru/nsu/kuzminov/BlackjackTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/kuzminov/BlackjackTest.java
@@ -1,6 +1,7 @@
 package ru.nsu.kuzminov;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
