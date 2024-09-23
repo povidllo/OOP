3133d2cd65aebcb9241c48053eac8f7f2dfee393
povidllo <kuzminovm78@gmail.com>
2024-09-23 13:52:33 +0700

    changes

--- Task_1_1_1/src/main/java/ru/nsu/kuzminov/HeapSort.java
+++ Task_1_1_1/src/main/java/ru/nsu/kuzminov/HeapSort.java
@@ -1,17 +1,19 @@
 package ru.nsu.kuzminov;
 
 
-public class Main {
+public class HeapSort {
 
     /**
-     * Sort array using heapSort
+     * Sort array using heapSort.
      *
-     * @param arr is an integer array
+     * @param arr is an integer array.
      */
-    static void heapSort(int[] arr) {
+    public static void heapSort(int[] arr) {
         int size = arr.length;
-        for (int i = size / 2 - 1; i >= 0; i--)
+        for (int i = size / 2 - 1; i >= 0; i--) {
             heapify(arr, i, size);
+        }
+
         for (int i = size - 1; i >= 0; i--) {
             int temp = arr[0];
             arr[0] = arr[i];
@@ -21,11 +23,11 @@ public class Main {
     }
 
     /**
-     * heapify checks if the subtree with root i is a heap
+     * heapify checks if the subtree with root i is a heap.
      *
-     * @param arr  is the integer the array
-     * @param i    is the root of the subtree
-     * @param size is the size of the subtree
+     * @param arr  is the integer the array.
+     * @param i    is the root of the subtree.
+     * @param size is the size of the subtree.
      */
     public static void heapify(int[] arr, int i, int size) {
         int l = i * 2 + 1;
@@ -48,6 +50,10 @@ public class Main {
         }
     }
 
+    /**
+     * main function.
+     * @param args command line arguments.
+     */
     public static void main(String[] args) {
 //        int[] testArr = {1, 2, 3, 4, 5, 6, 7};
 //        Main.heapify(testArr, 2, testArr.length);
