3c141bdd90b58ee3664af45a01e97b26f10effd3
povidllo <povidllo@users.noreply.github.com>
2024-10-25 18:00:46 +0000

    Deploying to gh-pages from @ povidllo/OOP@a25899c680da840db85aa9966dcec49adcc72178 🚀

---
+++ Task_1_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kuzminov","l":"GraphAdjList"},{"p":"ru.nsu.kuzminov","l":"GraphAdjMtx"},{"p":"ru.nsu.kuzminov","l":"GraphIncMtx"},{"p":"ru.nsu.kuzminov","l":"GraphInterface"}]
