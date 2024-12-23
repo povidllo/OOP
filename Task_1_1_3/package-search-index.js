33164780dc73fa982388ad9968cbd1afd54ff692
povidllo <povidllo@users.noreply.github.com>
2024-12-23 07:14:48 +0000

    Deploying to gh-pages from @ povidllo/OOP@9103b82d2dac6f17b3fb72f35c7c04ede4689dbe 🚀

--- Task_1_1_3/package-search-index.js
+++ Task_1_1_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.kuzminov"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.kuzminov"}];updateSearchResults();
