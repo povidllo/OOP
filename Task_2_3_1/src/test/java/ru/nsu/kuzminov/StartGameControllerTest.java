14112df412eacdc77fb10f7e3ad57963f0c7a2f3
povidllo <kuzminovm78@gmail.com>
2025-04-20 15:43:43 +0700

    please x3

--- Task_2_3_1/src/test/java/ru/nsu/kuzminov/StartGameControllerTest.java
+++ Task_2_3_1/src/test/java/ru/nsu/kuzminov/StartGameControllerTest.java
@@ -1,10 +1,10 @@
 package ru.nsu.kuzminov;
 
-import org.junit.jupiter.api.Test;
-import org.testfx.framework.junit5.ApplicationTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
-class StartGameControllerTest extends ApplicationTest {
+import org.junit.jupiter.api.Test;
+
+class StartGameControllerTest {
 
     @Test
     void validTest() {
